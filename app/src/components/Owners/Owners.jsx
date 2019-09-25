c355ab84d8713a3cc901cd065360a00e3714f6e1
Dustin Fedie <dlfedie@gmail.com>
2019-09-25 17:01:13 -0500

    adds base components

---
+++ app/src/components/Owners/Owners.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Owners extends Component {
+    render() {
+        return (
+            <h1>Owners component</h1>
+        )
+    }
+}
+
+export default Owners;
---
+++ app/src/components/Pets/Pets.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Pets extends Component {
+    render() {
+        return (
+            <h1>Pets component</h1>
+        )
+    }
+}
+
+export default Pets;
