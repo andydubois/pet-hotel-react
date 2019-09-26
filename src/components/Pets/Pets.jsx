53cf23eb05bc191d85c5c761776b2c8deea7f419
Miles Lacek <mileslacek@gmail.com>
2019-09-26 13:48:03 -0500

    Added to petSaga and petReducer.

--- src/components/Pets/Pets.jsx
+++ src/components/Pets/Pets.jsx
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class Pets extends Component {
+        componentDidMount() {
+        this.getPets();
+    }
+
+    
+    getPets = () => {
+        this.props.dispatch({
+            type: 'GET_PETS'
+        })
+    }
+    
     render() {
         return (
+            <>
+            {JSON.stringify{this.props.pets}}
             <h1>Pets component</h1>
+            <div>
+                <h2>Add Pet</h2>
+                <input placeholder="Pet Name"/>
+                <input placeholder="Pet Color"/>
+                <input placeholder="Pet Breed"/>
+                <select/>
+                <button>Submit</button>
+            </div>
+            <div>
+                <h2>History</h2>
+                <table>
+                    <thead>
+                        <th>
+                            Owner
+                        </th>
+                        <th>
+                            Pet
+                        </th>
+                        <th>
+                            Breed
+                        </th>
+                        <th>
+                            Color
+                        </th>
+                        <th>
+                            Checked In
+                        </th>
+                        <th>
+                            Actions
+                        </th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{this.props.pets}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            </>
         )
     }
 }
 
-export default Pets;
+const mapStateToProps = state => {
+    return {
+        pets: state.petsReducer
+    }
+}
+
+export default connect(mapStateToProps)(Pets);
--- src/redux/sagas/petSaga.jsx
+++ src/redux/sagas/petSaga.jsx
@@ -1,8 +1,23 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
-function* petSaga() {
+function* getPets(action) {
+    try{
+        yield console.log('in getOwners Saga');
+        let response = yield axios.get('/getPets.php');
+        yield put({
+            type: 'SET_PETS',
+            payload: response.data
+        })
+    } catch(err) {
+        console.log('error in getPets GET', err);
+        
+    }
+}
 
+
+function* petSaga() {
+    yield takeLatest('GET_PETS', getPets);
 }
 
 export default petSaga;
